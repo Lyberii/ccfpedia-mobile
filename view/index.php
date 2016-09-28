35f10a19e16f0f8795f2dccee1bd0c75d61f2604
刘旭利 <liuxuli@quyiyuan.com>
2016-09-28 20:13:18 +0800

    首页基本布局

--- view/index.php
+++ view/index.php
@@ -1,28 +1,67 @@
 <!DOCTYPE html>
 <html>
+
 <head lang="en">
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<!--极速模式-->
-<meta name="renderer" content="webkit"/>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>CCFpedia</title>
-<!--common-css-->
-<link rel="stylesheet" href="lib/bootstrap-3.3.6/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="lib/font-awesome/css/font-awesome.min.css"/>
-<!--jquery-->
-<script src="lib/jquery/jquery.min.js"></script>
-<script src="lib/jquery/jquery.inputmask.bundle.min.js"></script>
-<!-- load angular-->
-<script src="lib/angular-1.5.5/angular.min.js"></script>
-<script src="lib/angular-1.5.5/angular-animate.min.js"></script>
-<script src="lib/angular-1.5.5/angular-resource.min.js"></script>
-<script src="lib/bootstrap-3.3.6/js/bootstrap.min.js"></script>
-<script src="js/ccfmobile.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--极速模式-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CCFpedia</title>
+    <link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="../css/main.css" rel="stylesheet">
 </head>
-<body ng-app="main" ng-controller="mainCtrl">
-	<div class="container">
-		<div ng-model="test">{{content}}</div>
-	</div>
+<body>
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" 
+                        data-target="#example-nav-collapse">
+                    <span class="sr-only">切换导航</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#" style="width:80px;padding: 0">
+                    <img class="img-brand" alt="Brand" src="../images/ccflogo.png">
+                </a>
+                <div class="collapse navbar-collapse" id="example-nav-collapse">
+                    <ul class="nav navbar-nav nav-menu">
+                        <li class="active"><a href="/login">登录</a></li>
+                        <li><a href="/register">注册</a></li>
+                        <li class="divider"></li>
+                        <li><input type="text" class="form-control" placeholder="搜索"></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default">
+            <div class="panel-heading">
+                首页
+            </div>
+            <div class="panel-body">
+                <ul>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                    <li><a href="">计算机科学与技术</a></li>
+                </ul>
+            </div>
+            <div class="panel-footer">面板脚注</div>
+        </div>
+        </div>
+    </nav>
 </body>
+
 </html>
