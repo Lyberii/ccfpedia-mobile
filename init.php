30f5572f04b4ad9e4ef00febfe4b93e3a1ef319e
vForce <admin@qiujiahui.com>
2016-09-28 10:00:16 +0800

    fix bug

--- init.php
+++ init.php
@@ -2,6 +2,6 @@
 define('ROOT', __DIR__);
 define('IS_CLI', PHP_SAPI == 'cli');
 
-include_once 'lib\Request.php';
-include_once 'lib\Response.php';
-include_once 'lib\Util.php';
+include_once 'lib/Request.php';
+include_once 'lib/Response.php';
+include_once 'lib/Util.php';
