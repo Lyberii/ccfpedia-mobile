9c11708356083546685382d51c19e905c837a894
zxy <2326674367@qq.com>
2016-09-29 16:12:49 +0800

    Api_search

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?
 include_once "init.php";
+include_once 'lib/CCFApi.php';
 
 $request = Request::getInstance();
 $response = Response::getInstance();
 
 $path = $request->path();
+
+CCFApi::search('计',0);
+exit();
 
 $response->renderAndSend('index.php');
