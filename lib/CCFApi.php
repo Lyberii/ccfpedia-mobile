78aea4991e06d1e4625638209af40bb50051e396
vForce <admin@qiujiahui.com>
2016-09-30 21:01:22 +0800

    翻译表格

--- lib/CCFApi.php
+++ lib/CCFApi.php
@@ -61,11 +61,19 @@ class CCFApi {
 			}
 		}
 		//解析链接,ex:[[XXX]]
-		preg_match_all('/\[\[(.*?)\]\]/', $result, $matches);
-		if (isset($matches[1])) {
-			foreach ($matches[1] as $match) {
-				$result = str_replace("[[{$match}]]", "<a href='/{$match}'>{$match}</a>", $result);
-			}
+		$result = preg_replace('/\[\[(.*?)\]\]/', '<a href=\'/$1\'>$1</a>', $result);
+		//解析wikitable
+		preg_match_all('/{\|.*|}/s', $result, $matches);
+		foreach ($matches[0] as $wikitable) {
+			$tableDesctiption = substr($wikitable, strpos($wikitable, '{|') + 2, strpos($wikitable, '|-') - 2);
+			$htmlTable = "<table {$tableDesctiption}>";
+			$table = substr($wikitable, strpos($wikitable, '|-') + 2);
+			$table = preg_replace('/\|\|/', '</td><td>', $table);
+			$table = preg_replace('/\|\-/', '</td></tr>', $table);
+			$table = preg_replace('/\|}/', '</tr></table>', $table);
+			$table = preg_replace('/\|/', '<tr>', $table);
+			$htmlTable .= $table;
+			$result = str_replace($wikitable, $htmlTable, $result);
 		}
 		return $result;
 	}
