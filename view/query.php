e62132e215a7c1938a3612cc56fcca5dfa01352e
vForce <admin@qiujiahui.com>
2016-10-15 19:59:22 +0800

    remove unused files & compatible with /mobile/

--- view/query.php
+++ view/query.php
@@ -8,14 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
     <title>CCFpedia</title>
     <link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href="../css/main.css" rel="stylesheet">
+    <link href="/mobile/css/main.css" rel="stylesheet">
     <script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type="text/javascript">
         function goSearch(){
             var searchText = document.getElementById('search_value').value;
             var searchForm = document.getElementById('search_form');
-            searchForm.action = '/search/' + searchText + '/';
+            searchForm.action = '/mobile/search/' + searchText + '/';
             searchForm.submit();
         }
     </script>
@@ -32,7 +32,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="/" style="width:80px;padding: 0">
-                    <img class="img-brand" alt="Brand" src="../images/ccflogo.png">
+                    <img class="img-brand" alt="Brand" src="./images/ccflogo.png">
                 </a>
                 <div class="collapse navbar-collapse" id="example-nav-collapse">
                     <ul class="nav navbar-nav nav-menu">
--- view/search.php
+++ view/search.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
     <title>CCFpedia</title>
     <link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href="../css/main.css" rel="stylesheet">
+    <link href="/mobile/css/main.css" rel="stylesheet">
     <script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style type="text/css">
@@ -47,7 +47,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="/">
-                    <img class="img-brand" alt="Brand" src="../../images/ccflogo.png">
+                    <img class="img-brand" alt="Brand" src="/mobile/images/ccflogo.png">
                 </a>
                 <div class="collapse navbar-collapse" id="example-nav-collapse">
                     <ul class="nav navbar-nav nav-menu">
