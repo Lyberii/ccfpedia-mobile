0fc4f612cf4eef9f6c8518cd0e60948525072cea
vForce <admin@qiujiahui.com>
2016-10-16 19:08:40 +0800

    fix bug

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
 $keyword = urldecode(isset($path[2]) ? $path[2] : '');
 if (!$keyword) $response->renderAndSend('search.php', compact('keyword'));
 $queryResult = CCFApi::query($keyword);
-if ($queryResult) $response->redirect("/{$keyword}");
+if ($queryResult) $response->redirect("/mobile/{$keyword}");
 $offset = $request->offset ?: 0;
 $limit = $request->limit ?: 15;
 $searchResult = CCFApi::search($keyword, $offset, $limit);
