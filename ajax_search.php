dd5f4c4ab0816284bb473890c3e69d579f21ac6b
vForce <admin@qiujiahui.com>
2016-10-17 21:01:09 +0800

    compatible with jQuery UI selector

--- ajax_search.php
+++ ajax_search.php
@@ -7,6 +7,6 @@ $keyword = $request->s;
 $searchResult = CCFApi::search($keyword)['title'];
 $result = [];
 if ($searchResult) {
-	foreach ($searchResult as $item) $result[] = ['name' => $item['title']];
+	foreach ($searchResult as $item) $result[] = $item['title'];
 }
 $response->send(json_encode($result));
